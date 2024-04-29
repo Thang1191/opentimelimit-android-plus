acc1091508abd21591270742952b0143fb1d620d
Jonas Lochmann <git@inkompetenz.org>
2024-04-29 02:00:00 +0200

    Take a look at the room compiler warnings

--- app/src/main/java/io/timelimit/android/data/RoomDatabase.kt
+++ app/src/main/java/io/timelimit/android/data/RoomDatabase.kt
@@ -55,8 +55,9 @@ import java.util.concurrent.TimeUnit
     UserU2FKey::class,
     WidgetCategory::class,
     WidgetConfig::class
-], version = 30, autoMigrations = [
-    AutoMigration(from = 29, to = 30)
+], version = 31, autoMigrations = [
+    AutoMigration(from = 29, to = 30),
+    AutoMigration(from = 30, to = 31)
 ])
 abstract class RoomDatabase: RoomDatabase(), io.timelimit.android.data.Database {
     companion object {
